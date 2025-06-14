6a820edc13faade43101416ae1ea5cda5126dee5
MOHD_ARSATH_26 <167057703+mohamed-arsath-001@users.noreply.github.com>
2025-06-14 11:01:24 +0530

    Add files via upload

---
+++ analysis/analyze_src/missing_values_analysis.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+class MissingValuesAnalysisTemplate(ABC):
+    def analyze(self, df: pd.DataFrame):
+        self.identify_missing_values(df)
+        self.visualize_missing_values(df)
+
+    @abstractmethod
+    def identify_missing_values(self, df: pd.DataFrame):
+        pass
+
+    @abstractmethod
+    def visualize_missing_values(self, df: pd.DataFrame):
+        pass
+
+class SimpleMissingValuesAnalysis(MissingValuesAnalysisTemplate):
+    def identify_missing_values(self, df: pd.DataFrame):
+        print("\nMissing Values Count by Column:")
+        missing_values = df.isnull().sum()
+        print(missing_values[missing_values > 0])
+
+    def visualize_missing_values(self, df: pd.DataFrame):
+        print("\nVisualizing Missing Values...")
+        plt.figure(figsize=(12, 8))
+        sns.heatmap(df.isnull(), cbar=False, cmap="viridis")
+        plt.title("Missing Values Heatmap")
+        plt.show()
+
+
---
+++ run_pipeline.py
@@ -0,0 +1,19 @@
+import click
+from pipelines.training_pipeline import ml_pipeline
+from zenml.integrations.mlflow.mlflow_utils import get_tracking_uri
+
+
+@click.command()
+def main():
+    run = ml_pipeline()
+
+    print(
+        "Now run \n "
+        f"    mlflow ui --backend-store-uri '{get_tracking_uri()}'\n"
+        "To inspect your experiment runs within the mlflow UI.\n"
+        "You can find your runs tracked within the experiment."
+    )
+
+
+if __name__ == "__main__":
+    main()
